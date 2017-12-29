c701c59a45fe333debf2dd5b7057fd729f2a82ed
yuanyujin <3154971393@qq.com>
2017-12-29 11:21:56 +0800

    test

--- app/src/main/java/com/trsoft/app/mvpapplication/constant/Constant.java
+++ app/src/main/java/com/trsoft/app/mvpapplication/constant/Constant.java
@@ -7,4 +7,5 @@ package com.trsoft.app.mvpapplication.constant;
 public class Constant {
 
     public static  String BASE_URL="http://172.17.21.72:9090/api/v1/customer/";
+    public static String aa;
 }
